10abb09e10ff504c5ab37f9ebf0b8f06f5800796
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-09-14 00:11:31 +0530

    remove event check

--- tmp/packages/size-plugin-store/index.js
+++ tmp/packages/size-plugin-store/index.js
@@ -6,7 +6,7 @@ const SIZE_STORE_ENDPOINT =
   process.env.SIZE_STORE_ENDPOINT || "https://size-store.now.sh";
 
 async function publishDiff(diff, filename) {
-  if (process.env.NODE_ENV !== "test" && ci && event == "pull_request") {
+  if (process.env.NODE_ENV !== "test" && ci ) {
     try {
       const params = { ci, repo, branch, sha, filename, diff };
       await axios.post(`${SIZE_STORE_ENDPOINT}/diff`, params);
@@ -16,7 +16,7 @@ async function publishDiff(diff, filename) {
   }
 }
 async function publishSizes(size, filename) {
-  if (process.env.NODE_ENV !== "test" && ci && event == "push") {
+  if (process.env.NODE_ENV !== "test" && ci) {
     try {
       const params = { ci, repo, branch, sha, filename, size };
       await axios.post(`${SIZE_STORE_ENDPOINT}/size`, params);
